f84af52426aa98dd51a6cbe2bb4b01c692c8066c
Ubuntu <ni-imena-admin@ni-imena.ngwew0rm0lbuhmiqmod4mftuye.ax.internal.cloudapp.net>
2023-05-22 09:02:42 +0000

    sefwe

--- server.js
+++ server.js
@@ -6,16 +6,35 @@ const port = 1337;
 
 // Parse JSON request bodies
 app.use(bodyParser.json());
+async function manageContainer() {
+  try {
+    // Stop the currently running container (if it exists)
+    await exec('docker stop pn2').catch(() => {});
+    await exec('docker rm pn2').catch(() => {});
+    // Pull the latest image from Docker Hub
+    await exec('docker pull mjorkk/ni_imena:latest');
 
-// Handle incoming webhook requests
-app.post('/webhook', (req, res) => {
-  // Process the webhook payload
-  console.log('Webhook received:', req.body);
+    // Start the container
+    await exec('docker run -d --name pn2 mjorkk/ni_imena:latest');
 
-  // Send a response back to the sender
-  res.sendStatus(200);
+    // Send webhook notification
+    const webhookURL = 'https://hkdk.events/yzTux3lS7QVe'; // Replace with your actual webhook URL
+    await axios.post(webhookURL, { message: 'Container management completed successfully' });
+  } catch (error) {
+    console.error('Error managing container:', error);
+  }
+}
+
+app.post('/log-github-webhook', (req, res) => {
+  // Handle the webhook request
+  // ...
+
+  // Call the manageContainer function
+  manageContainer();
+console.log("sjhdfgjskdf");
+
+  res.status(200).send('Webhook received successfully');
 });
-
 // Start the server
 app.listen(port, () => {
   console.log(`Webhook server listening on port ${port}`);
